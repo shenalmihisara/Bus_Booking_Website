ba78dae61c9ecbb02276019f70e73af0f1aeab2c
shenalmihisara <shenalmihisara51@gmail.com>
2026-08-10 19:54:39 +0530

    Implement login functionality and update UI for sign-up and login pages

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const requestOptions = {
-  method: "GET",
-  redirect: "follow"
-};
+function btnLoginOnAction(){
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-fetch("https://api.freeprojectapi.com/api/BusBooking/GetAllUsers", requestOptions)
-  .then((response) => response.json())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+  const raw = JSON.stringify({
+    "userName": "string",
+    "password": "string"
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  fetch("https://api.freeprojectapi.com/api/BusBooking/login", requestOptions)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+    
+}
